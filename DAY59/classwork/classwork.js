346468ded3ccfa41de557529326446497dc262ef
group5roba <168303629+group5roba@users.noreply.github.com>
2024-05-04 19:45:47 +0400

    Delete DAY59/classwork directory

--- DAY59/classwork/classwork.js
+++
@@ -1,9 +0,0 @@
-console.log('1' + 1)// გამოიტანს 11
-console.log('1' - 1)// გამოიტანს 0
-console.log('1' * 1)// გამოიტანს 1
-console.log('1' / 1)// გამოიტანს 1
-
-console.log(parseINt('1' + 1))
-console.log(Number('1' - 1))
-console.log(String('1' * 1))
-console.log(parseFloat('1' / 1))
